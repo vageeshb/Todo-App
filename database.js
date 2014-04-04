171a12e102c20b9cacc3145711ef1e97dc0cf2cf
VageeshB <vageesh_bhasin@outlook.com>
2014-04-04 11:59:31 +0530

    Final touches

--- database.js
+++ database.js
@@ -1,10 +1,11 @@
 // database.js
 
-var mongoose 	= 	require('mongoose');
+var mongoose  =   require('mongoose');
 
-var todoSchema 	= mongoose.Schema({
-	name 	: 	{ type: String, required: true},
-	status 	: 	Boolean
+var todoSchema  = mongoose.Schema({
+  name  :   { type: String, required: true},
+  status  :   Boolean
 });
 
+// CREATING AND EXPOSING OUR MODEL TO THE APP
 module.exports = mongoose.model('Todo', todoSchema);
--- server.js
+++ server.js
@@ -1,92 +1,92 @@
 // server.js
 
 // LOADING REQ MODULES
-var express 	= 	require('express')
-	, path 			= 	require('path')
-	, app 			= 	express()
-	, mongoose 	= 	require('mongoose')
-	, Todo 			= 	require('./database.js')
-	;
+var express   =   require('express')
+  , path      =   require('path')
+  , app       =   express()
+  , mongoose  =   require('mongoose')
+  , Todo      =   require('./database.js')                    // Holds our schema and model
+  ;
 
 
 // DATABASE CONNECTION
 mongoose.connect('mongodb://localhost:27017');
 
-// CONFIG
+var port      =   process.env.PORT || 8080;
 
-var port 			= 	process.env.PORT || 8080;
-
+// APP CONFIG
 app.configure( function() {
 
-	// VIEWS FOLDER and TEMPLATING ENGINE
-	app.set('views', path.join(__dirname, 'views'));
-	app.set('view engine', 'jade');
+  // VIEWS FOLDER and TEMPLATING ENGINE
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'jade');
 
-	// MIDDLEWARE
-	app.use(express.logger()); // Logging server requests
-	app.use(express.urlencoded());
-	app.use(express.json());
-	app.use(express.methodOverride());
-	app.use(express.static(__dirname + '/public/stylesheets')); // Server stylesheets
-	app.use(app.router);			// Router to handle routes
-	
+  // MIDDLEWARE
+  app.use(express.logger());                                  // Logging server requests
+  app.use(express.urlencoded());
+  app.use(express.json());
+  app.use(express.methodOverride());
+  app.use(express.static(__dirname + '/public/stylesheets')); // Server stylesheets
+  app.use(app.router);                                        // Router to handle routes
+  
 });
 
 // ROUTES
 // TODO ROUTES ==================================
-// READ TODOS
+// READ TASKS
 app.get('/', function(req, res) {
-	Todo.find({}, function(err, data) {
-		if(err) throw err;
-		res.render('index.jade', {
-			todos 	: 	data 
-		});	
-	});
+  Todo.find({}, function(err, data) {
+    if(err) throw err;
+    res.render('index.jade', {
+      todos   :   data 
+    }); 
+  });
 });
 
-// CREATE TODO
+// CREATE TASK
 app.post('/', function(req, res) {
-	var newTodo 		= 	new Todo();
-	newTodo.name 		= 	req.body.task;
-	newTodo.status 	= 	false;
-	newTodo.save(function(err, task) {
-		if(err) {
-			throw err;
-		} else {
-			res.redirect('/');
-		}
-	});
+  var newTodo     =   new Todo();
+  newTodo.name    =   req.body.task;
+  newTodo.status  =   false;
+  newTodo.save(function(err, task) {
+    if(err) {
+      throw err;
+    } else {
+      res.redirect('/');
+    }
+  });
 });
 
-// DELETE TODO
+// DELETE TASK
 app.get('/:id/delete', function(req, res) {
-	Todo.findByIdAndRemove(req.params.id, function(err) {
-		if(err) throw err;
-		res.redirect('/');
-	});
+  Todo.findByIdAndRemove(req.params.id, function(err) {
+    if(err) throw err;
+    res.redirect('/');
+  });
 });
 
-// UPDATE TODO
+// UPDATE TASK - MARK AS DONE
 app.get('/:id/do', function(req, res) {
-	Todo.findByIdAndUpdate(req.params.id, { status: true } , function(err) {
-		if(err) throw err;
-		res.redirect('/');
-	});
+  Todo.findByIdAndUpdate(req.params.id, { status: true } , function(err) {
+    if(err) throw err;
+    res.redirect('/');
+  });
 });
 
+// UPDATE TASK - MARK AS NOT DONE
 app.get('/:id/undo', function(req, res) {
-	Todo.findByIdAndUpdate(req.params.id, { status: false } , function(err) {
-		if(err) throw err;
-		res.redirect('/');
-	});
+  Todo.findByIdAndUpdate(req.params.id, { status: false } , function(err) {
+    if(err) throw err;
+    res.redirect('/');
+  });
 });
 // ==============================================
 
 app.get('*', function(req, res) {
-	res.redirect('/');
+  res.render('index.jade');
 });
 
 // SERVER LAUNCH
 app.listen( port, function() {
-	console.log('Server is running and listening to: ' + port);
+  console.log('Server is running and listening to: ' + port);
 });
